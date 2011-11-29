e6cd8e493f4a925422b53d280da1d856bdf9c8db
141061 <141061@943e2570-971a-e111-986e-005056870011>
2011-11-29 14:39:26 +0000

    Created project: get all local databases and display in drop-down box.

---
+++ PG2GX/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.OleDb;
+
+using Npgsql;
+
+namespace PG2GX
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            String conn = "Server=127.0.0.1;Port=5432;Integrated Security=true;User Id=postgres;Password=postgres";
+
+
+
+
+            NpgsqlConnection sqlConx;
+            try
+            {
+                sqlConx = new NpgsqlConnection(conn);
+                sqlConx.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return;
+            }
+                
+            DataTable tblDatabases = sqlConx.GetSchema("Databases");
+            sqlConx.Close();
+
+            foreach (DataRow row in tblDatabases.Rows)
+            {
+                //Console.WriteLine("Database: " + row["database_name"]);
+                this.comboBox1.Items.Add(row["database_name"]);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("nichts ausgewählt!");
+                return;
+            }
+        }
+    }
+}
